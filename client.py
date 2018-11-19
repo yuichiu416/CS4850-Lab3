2f5f8503761d5710f81d64e3b9206115075cbff1
Roger Kiew <yuichiu416@gmail.com>
2018-11-19 02:36:18 -0600

    finished all functions except for send UserID

--- client.py
+++ client.py
@@ -1,21 +1,38 @@
-import socket
+# Roger Kiew, 14283381, rkry8
+# CS 4850 Lab 3 version 2
+# Multiuser chatroom
+# Client script
+import socket, threading, sys
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 13381
 
-sock.connect((host, port))
-print ('Connected to', host)
+try:
+    sock.connect((host, port))
+    print ('Connected to', host)
+except:
+    print('Unable to connect\n')
+    sys.exit()
 
-while (True):
-    msg = input('> ')
+clientRunning = True
+
+def receiveMsg(sock):
+    serverDown = False
+    while clientRunning and (not serverDown):
+        try:
+            msg = sock.recv(1024).decode('utf-8')
+            print(msg)
+        except:
+            print('Server is Down. You are now Disconnected. Press enter to exit...')
+            serverDown = True
+
+threading.Thread(target = receiveMsg, args = (sock,)).start()
+
+while (clientRunning):
+    msg = input()
     sock.sendall(msg.encode('utf-8'))
-    # Halts
-    returnMsg = (sock.recv(1024)).decode('utf-8')
-    print ('> ' + returnMsg)
-
     if (msg == ':q'):
         break
-    ### End if z == ':q':
-    #elif (msg == 'logout' and returnMsg != 'No user is currently logged in.'):
-    #    break
+print("Quiting, shutting down...")
+sock.close()
--- server.py
+++ server.py
@@ -1,178 +1,221 @@
-import socket
-from threading import Thread
+# Roger Kiew, 14283381, rkry8
+# CS 4850 Lab 3 version 2
+# Multiuser chatroom
+# Server script
 
+import socket, threading
+
+# global variables
 users = []
 passwords = []
-activeUsers = []
-loginInfo = {}
-clients = {}
-addresses = {}
-
-with open('users.txt') as file:
-    for line in file:
-        line = line.replace("\n", "")
-        fields = line.split(' ')
-        users.append(fields[0])
-        passwords.append(fields[1])
-sock = socket.socket()
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-host = socket.gethostname()
-port = 13381
-sock.bind((host, port))
-MAXCLIENTS = 3
-
-sock.listen(MAXCLIENTS)
-conn = None
-
+activeUsers = {}
+serverRunning = True
 currUser = ''
-'''def accept_incoming_connections():
-    while True:
+clientAddresses = {}
+conns = {}
+sock = ''
+host = ''
+port = ''
+MAXCLIENTS = 3
+conn = ''
+loggedIn = False
+def getCurrentUser(conn):
+    for key in activeUsers:
+        if(conn == activeUsers[key]):
+            return key
 
-        print("running")
-        client, client_address = sock.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the Roger! Now type your name and password and press enter!", "utf8"))
-        # addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+def isLoggedIn(conn):
+    for key in activeUsers:
+        if(activeUsers[key] == conn):
+            return True
+    return False
 
-def client_thread(conn):
-    conn.send("Welcome to the Server. Type messages and press enter to send.\n")
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        reply = "OK . . " + data
-        conn.sendall(reply)
-    conn.close()
+def reloadLoginFile():
+    global  users , passwords, activeUsers
+    with open('users.txt', 'r') as file:
+        for line in file:
+            if(line):
+                line = line.replace("\n", "")
+                line = line.replace("\r", "")
+                fields = line.split(' ')
+                if(fields[0] not in users):
+                    users.append(fields[0])
+                if(fields[1] not in passwords):
+                    passwords.append(fields[1])
+                if(fields[0] not in activeUsers):
+                    activeUsers.update({fields[0]: ''})
+        file.close()
 
+def sendMsg(conn, msg):
+    conn.send(msg.encode('utf-8'))
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+def setup():
+    global sock, host, port
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host = socket.gethostname()
+    port = 13381
+    sock.bind((host, port))
+    MAXCLIENTS = 3
+    sock.listen(MAXCLIENTS)
 
-    name = client.recv(1024).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type :q to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name + ": ")
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8") + msg)
-'''
 def login(conn, cmd):
-    global currUser, activeUsers
-    for i in range(0, len(users)):
+    global activeUsers, addr
+    i = 0
+    reloadLoginFile()
+    for key in activeUsers:
+        if(conn == activeUsers[key]):
+            msg = 'Please log out first.'
+            sendMsg(conn, msg)
+            return msg
+    for key in activeUsers:
+        if(cmd[1] == key and activeUsers[key] != ''):
+            msg = 'User ' + key + ' is already logged in. Please logout first if you wish to login as a differnt user.'
+            sendMsg(conn, msg)
+            return msg
         if (users[i] == cmd[1] and passwords[i] == cmd[2]):
-            response = 'Server: ' + users[i] + ' joins.'
             currUser = users[i]
-            activeUsers.append(currUser)
-            print("activeUsers added: ", currUser)
-            print(currUser + ' login.')
-            return response
-    if (currUser != True):
-        return 'Incorrect UserID/Password.'
+            activeUsers.update({currUser: conn})
+            print('list in login', activeUsers)
+            msg = 'logged in.'
+            for key in activeUsers:
+                if(conn == activeUsers[key]):
+                    sendMsg(activeUsers[key], msg)
+            return currUser +' ' + msg
+        i += 1
+    msg = 'Incorrect UserID/Password.'
+    for key in activeUsers:
+        if(activeUsers[key] != '' and conn == activeUsers[key]):
+            sendMsg(activeUsers[key], msg)
+    msg = 'Bad login attempt from: ' + host
+    sendMsg(conn, msg)
+    return msg
+
 def newuser(conn, cmd):
-        createNewUser = True
-        # for user in users:
-        for i in range(1, len(users)):
-            # if (user['UserID'] == cmd[1]):
-            if (users[i] == cmd[1]):
-                response = 'Server: ' + users[i] + ' already exists. Please try a different username.'
-                conn.send(response.encode('utf-8'))
-                createNewUser = False
-                break
-        if (createNewUser == True):
-            if (len(cmd[1]) < 32 and len(cmd[2]) >= 4 and len(cmd[2]) <= 8):
-                # users.append({'UserID' : cmd[1], 'Password' : cmd[2]})
-                with open("users.txt", "a") as userFile:
-                    userFile.write(cmd[1] + ' ' + cmd[2] + "\n")
-                    userFile.close()
-                response = 'User ' + cmd[1] + ' created successfully.'
-                return response
-            else:
-                return 'The length of the UserID should be less than 32, and the length of the Password should be between 4 and 8 characters.'
+    createdUser = False
+    reloadLoginFile()
+    for i in range(0, len(users)):
+        if (users[i] == cmd[1]):
+            msg = 'Server: ' + users[i] + ' already exists. Please try a different username.'
+            sendMsg(conn, msg)
+            createNewUser = False
+            return msg
+    if (len(cmd[1]) < 32 and len(cmd[2]) >= 4 and len(cmd[2]) <= 8):
+        with open('users.txt', 'r') as file:
+            lines = file.read()
+            file.close()
+            with open('users.txt', 'w') as file:
+                print(cmd[1], cmd[2])
+                file.write(lines)
+                file.write('\n' + cmd[1] + ' ' + cmd[2])
+                file.close()
+
+        msg = 'User ' + cmd[1] + ' created successfully by' + host
+        sendMsg(conn, msg)
+        createdUser = True
+        return msg
+    else:
+        msg = 'The length of the UserID should be less than 32, and the length of the Password should be between 4 and 8 characters.'
+        sendMsg(conn, msg)
+        return ''
+
 def sendAll(conn, cmd):
-    cmd.remove(cmd[0])
-    cmd.remove(cmd[1])
-    msg = ' '.join(cmd)
-    msg = "<sending all>: " + currUser + ': ' + msg
-    conn.sendall(msg.encode('utf-8'))
+    global activeUsers
+    cmd.remove(cmd[0]) # before: send  all <message>
+    cmd.remove(cmd[0]) # before: all <message>
+    msg = ' '.join(cmd) # <message>
+    msg = currUser + "<sending all>" + ': ' + msg
+    for key in activeUsers:
+        if(activeUsers[key]):
+            sendMsg(activeUsers[key], msg)
     print(msg)
     return ''
+
 def send(conn, cmd):
     cmd.remove(cmd[0])
     msg = ' '.join(cmd)
     msg = currUser + ': ' + msg
-    print('msg is', msg)
+    for key in activeUsers:
+        if(activeUsers[key]):
+            sendMsg(activeUsers[key], msg)
     return msg
 
-def who():
-    pass
-
-def logout():
-    global currUser, activeUsers
-    msg = 'Server: ' + currUser + ' left.'
-    conn.send(msg.encode('utf-8'))
-    print(currUser + ' logout.')
-    activeUsers.remove(currUser)
-    print('removed', currUser, 'from active users')
-    currUser = ''
+def who(conn, cmd):
+    global activeUsers
+    reloadLoginFile()
+    currUser = getCurrentUser(conn)
+    msg = 'Users in the chatroom: '
+    sendMsg(conn, msg)
+    for key in activeUsers:
+        if(activeUsers[key] != ''):
+            sendMsg(activeUsers[currUser], key)
     return ''
+
+def logout(conn):
+    global activeUsers
+    for key in activeUsers:
+        if(conn == activeUsers[key]):
+            activeUsers.update({key : ''})
+            msg = key + ' left'
+            sendMsg(conn, msg)
+    return msg
 
 def switcher(conn, msg):
     cmd = msg.split()
     if len(cmd) > 2 and cmd[0] == "login":
-        if currUser != '':
-            return 'User ' + currUser + ' is already logged in. Please logout first if you wish to login as a differnt user.'
         return login(conn, cmd)
     elif len(cmd) > 2 and cmd[0] == "newuser":
         return newuser(conn, cmd)
-    elif len(cmd) > 3 and cmd[0] == "send" and cmd[1] == "all":
-        if (currUser == ''):
-            return 'Denied. Please login in first.'
+    if (not isLoggedIn(conn)):
+        msg = 'Denied. Please login in first.'
+        return sendMsg(conn, msg)
+    elif len(cmd) > 2 and cmd[0] == "send" and cmd[1] == "all":
         return sendAll(conn, cmd)
-    elif len(cmd) > 2 and cmd[0] == "send":
-        if (currUser == ''):
-            return 'Denied. Please login in first.'
+    elif len(cmd) > 1 and cmd[0] == "send":
         return send(conn, cmd)
-    elif len(cmd) == 1 and cmd == "who":
+    elif len(cmd) == 1 and cmd[0] == "who":
         return who(conn, cmd)
     elif cmd[0] == "logout":
-        if (currUser == ''):
-            return "No user is currently logged in."
-        return logout()
+        return logout(conn)
     else:
-        return "Unknown command, or incorrect usage of command. Please try again."
+        msg = "Unknown command, or incorrect usage of command. Please try again."
+        return sendMsg(conn, msg)
 
-while True:
-    if (conn is None):
+def handleClient(conn, addr):
+    global activeUsers
+    clientConncted = True
+    while(clientConncted):
+        try:
+            msg = (conn.recv(1024)).decode('utf-8')
+            msg = switcher(conn, msg)
+            print(msg)
+        except:
+            print("there's an error, please check the connection.")
+            clientConncted = False
+            if(len(activeUsers) != 0):
+                try:
+                    activeUsers.update({currUser, ''})
+                except:
+                    print("There's an error, please restart the program")
+
+def runServer():
+    global host, port, conn, addr
+    while(serverRunning):
         print('Running on host:', host, 'port: ', port)
         print('[Waiting for connection...]')
         conn, addr = sock.accept()
         print('Got connection from', addr)
-    else:
         print("[-] Connected to " + addr[0] + ":" + str(addr[1]))
-        # start_new_thread(client_thread, (conn,))
-        try:
-            msg = (conn.recv(1024)).decode('utf-8')
-            msg = switcher(conn, msg)
-            conn.send(msg.encode('utf-8'))
-            print(msg)
-        except:
-            print("there's an error, please check the connection.")
-sock.close()
+
+        if(addr not in clientAddresses):
+            clientAddresses[addr] = addr
+            conns[conn] = conn
+            threading.Thread(target= handleClient, args = (conn, addr)).start()
+
+def main():
+    setup()
+    runServer()
+    sock.close()
+
+if __name__ == "__main__":
+    main()
